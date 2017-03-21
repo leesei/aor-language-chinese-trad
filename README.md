efe902d37017e16e9f8d1726ec1841d136536439
leesei <leesei@gmail.com>
2017-03-21 14:08:09 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# aor-language-chinese-traditional
+
+Credits:
+[Admin-on-rest](https://marmelab.com/admin-on-rest/)
+[downup2u/aor-language-chinese](https://github.com/downup2u/aor-language-chinese)
---
+++ index.js
@@ -0,0 +1,63 @@
+module.exports = {
+    aor: {
+        action: {
+            delete: '刪除',
+            show: '顯示',
+            list: '列表',
+            save: '保存',
+            create: '新建',
+            edit: '編輯',
+            cancel: '取消',
+            refresh: '刷新',
+            add_filter: '添加過濾',
+            remove_filter: '刪除過濾',
+        },
+        boolean: {
+            true: '是',
+            false: '否',
+        },
+        page: {
+            list: '%{name} 列表',
+            edit: '%{name} #%{id}',
+            show: '%{name} #%{id}',
+            create: '新建 %{name}',
+            delete: '刪除 %{name} #%{id}',
+        },
+        input: {
+            image: {
+                upload_several: '拖放多個檔案或點擊上傳.',
+                upload_single: '拖放一個檔案或點擊上傳.',
+            },
+        },
+        message: {
+            yes: '是',
+            no: '否',
+            are_you_sure: '你確定?',
+            about: '關於',
+        },
+        navigation: {
+            page_out_of_boundaries: '頁%{page}不在範圍內',
+            page_out_from_end: '超過最後一頁',
+            page_out_from_begin: '超出第一頁',
+            page_range_info: '%{offsetBegin}-%{offsetEnd} 共 %{total}',
+            next: '下一頁',
+            prev: '上一頁',
+        },
+        auth: {
+            username: '用戶名',
+            password: '密碼',
+            sign_in: '登入',
+            sign_in_error: '驗證失敗,請重試',
+            logout: '登出',
+        },
+        notification: {
+            updated: '已更新',
+            created: '已新建',
+            deleted: '已刪除',
+            item_doesnt_exist: 'L\'項目\'不存在',
+        },
+        validation: {
+            required: '必須',
+        },
+    },
+};
